c3e14bfbc54968b42881c0d2757e4bba46f7818f
suchintannit <suchintan.mishra@gmail.com>
2019-01-01 21:52:06 -0800

    Update Handler.java

--- src/getwelcome/src/main/java/Handler.java
+++ src/getwelcome/src/main/java/Handler.java
@@ -1,9 +1,22 @@
 import java.util.HashMap;
+import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.net.URISyntaxException;
 
 public class Handler {
-    public Object handler(Object message) {
+    public Map handler(Map message) throws IOException, URISyntaxException {
         System.out.println(message);
+        String responseBody = new String(Files.readAllBytes(Paths.get(getClass().getResource("welcome.html").toURI())));
 
-        return new HashMap();
+        HashMap response = new HashMap();
+        HashMap headers = new HashMap();
+
+        response.put("statusCode", "200");
+        headers.put("Content-Type", "text/html");
+        response.put("headers", headers);
+        response.put("body", responseBody);
+        return response;
     }
 }
